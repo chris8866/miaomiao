0545d011fb5cf1032b09e250711f7524aeef0c38
Chris Hu <qmsyl@sina.com>
2019-08-08 14:13:27 +0800

    bug clear

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -6,7 +6,7 @@
     <Loading v-if="isLoading" />
     <div v-else id="content" class="contentDetail">
       <div class="detail_list">
-        <div class="detail_list_bg"></div>
+        <div class="detail_list_bg" :style="{ 'background-image' : 'url('+detailMovie.img.replace(/w\.h/,'148.208')+')'}"></div>
         <div class="detail_list_filter"></div>
         <div class="detail_list_content">
           <div class="detail_list_img">
@@ -88,7 +88,7 @@ export default {
 }
 #content.contentDetail{display:block; margin-bottom:0;}
 #content .detail_list{height:200px; width:100%; position:relative; overflow: hidden;}
-.detail_list .detail_list_bg{width:100%; height:100%; background:url() 0 40%; filter:blur(20px);}
+.detail_list .detail_list_bg{width:100%; height:100%; background:0 40%; filter:blur(20px); background-size:cover; position:absolute; left:0; top:0}
 .detail_list .detail_list_filter{width:100%; height:100%; position:absolute; background-color:#40454d; opacity:.55}
 .detail_list .detail_list_content{display:flex; width:100%; height:100%; position: absolute; left:0; top:0; z-index: 2;}
 .detail_list .detail_list_img{width:108px; height:150px; border:1px solid #f0f2f3; margin:20px}
